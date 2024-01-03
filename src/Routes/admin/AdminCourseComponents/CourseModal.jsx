a07688f419e80505e094d6030e5f98c0c0339ffb
jyp <sndi9207@naver.com>
2024-01-03 11:33:55 +0900

    Feat: 관리자 페이지 검색 등록 완료

--- src/Routes/admin/AdminCourseComponents/CourseModal.jsx
+++ src/Routes/admin/AdminCourseComponents/CourseModal.jsx
@@ -1,79 +1,140 @@
 import React, { useState, useEffect } from 'react';
 import { Dialog, DialogTitle, DialogContent, DialogActions, TextField, Button, FormControl, InputLabel, Select, MenuItem} from '@mui/material';
-import { MobileDatePicker } from '@mui/x-date-pickers';
-
-import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { fetchGolfNames, postCourse, updateCourse } from '../../../services/admin/GolfCourse'; // 이름 변경 가져오기 골프장 no 에 대한 이름 수정 쿼리 
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 
-function GolfCourseModal({ open, onClose, course, onSave, onDelete, onCreate }) {
-  const [golfCourseData, setGolfCourseData] = useState(course || {});
+import dayjs from 'dayjs';
 
+import { TimePicker } from '@mui/x-date-pickers/TimePicker';
+
+function GolfCourseModal({ open, onClose, course, onUpdate, onDelete, onPost  }) {
+  // 초기값에 golf_date를 null로 설정
+  const [golfCourseData, setGolfCourseData] = useState(course || {
+    golf_date: null,
+    golf_time: null,
+    course_name: null, // 초기값 설정
+    greenpee: null,    // 초기값 설정
+    // golf_status: 0,  // 예약 상태를 0으로 설정
+    // 다른 필드들에 대해서도 초기값을 설정하세요.
+  });
+  
+  const [golfNames, setGolfNames] = useState([]); // 골프장 이름 목록
+// golf_date 값을 변경할 때는 별도의 상태로 관리
+  
   useEffect(() => {
-    setGolfCourseData(course || {});
+    // course가 null일 때 golf_date를 null로 설정
+    setGolfCourseData(course || { golf_date: null });
+    fetchGolfNamesData(); // 골프장 이름 데이터 가져오기
   }, [course]);
 
-  const handleChange = (event) => {
-    const { name, value } = event.target;
-    setGolfCourseData({ ...golfCourseData, [name]: value });
-  };
+ // 골프장 이름 데이터를 가져오는 함수
+ const fetchGolfNamesData = async () => {
+  const data = await fetchGolfNames();
+  setGolfNames(data);
+};
+const handleChange = (event) => {
+  const { name, value } = event.target;
+  const newValue = name === "golf_status" ? value : (value || '');
+  setGolfCourseData({ ...golfCourseData, [name]: newValue });
+};
 
-  const handleSave = () => {
-    if (golfCourseData.golf_no) {
-      onSave(golfCourseData);
+  const handleSave = async () => {
+  try {
+    if (golfCourseData.course_no) {
+      // 이미 있는 코스 번호가 있는 경우 (수정)
+      const response = await updateCourse(golfCourseData.course_no, golfCourseData);
+      console.log("골프장 정보 수정 완료:", response);
+      // 데이터 업데이트 후 페이지 새로 고침
+      window.location.reload();
     } else {
-      onCreate(golfCourseData);
+      // 새로운 골프장을 생성하는 경우
+      const response = await postCourse(golfCourseData);
+      console.log("새로운 골프장 생성 완료:", response);
+      // 데이터 업데이트 후 페이지 새로 고침
+      window.location.reload();
     }
-    onClose();
-  };
-
+  } catch (error) {
+    console.error("골프장 정보 처리 실패:", error);
+  }
+  onClose();
+};
   const handleDelete = () => {
     onDelete(golfCourseData.golf_no);
     onClose();
   };
-
+  const handleDateChange = (newDate) => {
+    setGolfCourseData((prevData) => ({
+      ...prevData,
+      golf_date: dayjs(newDate).format("YYYY-MM-DD"), // 날짜를 "YYYY-MM-DD" 형식으로 변환
+    }));
+  };
+  
+  const handleTimeChange = (newTime) => {
+    setGolfCourseData((prevData) => ({
+      ...prevData,
+      golf_time: dayjs(newTime).format("HH:mm"), // 시간을 "HH:mm" 형식으로 변환
+    }));
+  };
+console.log(golfCourseData);
   return (
     <Dialog open={open} onClose={onClose}>
-      <DialogTitle>{golfCourseData.golf_no ? '골프장 수정' : '신규 골프장 추가'}</DialogTitle>
+      <DialogTitle>골프장 상세페이지</DialogTitle>
       <DialogContent>
+      <FormControl fullWidth>
+          <InputLabel id="golf-no-label">골프장</InputLabel>
+          <Select
+            labelId="golf-no-label"
+            id="golf_no"
+            name="golf_no"
+            value={golfCourseData.golf_no || ''}
+            label="골프장"
+            onChange={handleChange}
+          >
+            {golfNames.map((golf) => (
+              <MenuItem key={golf.golf_no} value={golf.golf_no}>
+                {golf.name}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
         <TextField
-          margin="dense"
-          name="name"
-          label="골프장 이름"
-          type="text"
-          fullWidth
-          variant="standard"
-          value={golfCourseData.name || ''}
-          onChange={handleChange}
-        />
-        <TextField
-          margin="dense"
-          name="region"
-          label="코스 이름"
-          type="text"
-          fullWidth
-          variant="standard"
-          value={golfCourseData.region || ''}
-          onChange={handleChange}
-        />
-        <TextField
-          margin="dense"
-          name="greenfee"
-          label="그린피 금액"
-          type="number"
-          fullWidth
-          variant="standard"
-          value={golfCourseData.greenfee || ''}
-          onChange={handleChange}
-        />
-        <LocalizationProvider dateAdapter={AdapterDateFns}>
-          <MobileDatePicker
+              margin="dense"
+              name="course_name"
+              label="코스 이름"
+              type="text"
+              fullWidth
+              variant="standard"
+              value={golfCourseData.course_name || ''}
+              onChange={handleChange}
+            />
+            <TextField
+              margin="dense"
+              name="greenpee"
+              label="그린피 금액"
+              type="number"
+              fullWidth
+              variant="standard"
+              value={golfCourseData.greenpee || ''}
+              onChange={handleChange}
+            />
+         <LocalizationProvider dateAdapter={AdapterDayjs}>
+         <DatePicker
             label="예약 날짜"
-            inputFormat="yyyy/MM/dd"
-            value={golfCourseData.golf_date}
-            onChange={(newValue) => {
-              setGolfCourseData({ ...golfCourseData, golf_date: newValue });
-            }}
-            renderInput={(params) => <TextField {...params} />}
+            format="YYYY/MM/DD"
+            value={golfCourseData.golf_date || null} // 초기값을 빈 문자열로 설정
+            onChange={handleDateChange}
+            inputFormat={"yyyy-MM-dd"}
+            mask={"____-__-__"}
+          />
+          <TimePicker
+            label="티오프 시간"
+            ampm={false}
+            value={golfCourseData.golf_time || null} // 초기값을 빈 문자열로 설정
+            onChange={handleTimeChange}
+            inputFormat={"hh:mm"}
+            mask={"__:__"}
           />
         </LocalizationProvider>
         <FormControl fullWidth margin="dense">
@@ -84,20 +145,20 @@ function GolfCourseModal({ open, onClose, course, onSave, onDelete, onCreate }) 
             name="golf_status"
             value={golfCourseData.golf_status || ''}
             label="예약 상태"
+           
             onChange={handleChange}
           >
-            <MenuItem value={1}>예약 가능</MenuItem>
             <MenuItem value={0}>예약 불가</MenuItem>
+            <MenuItem value={1}>예약 가능</MenuItem>
           </Select>
         </FormControl>
-        {/* Additional input fields for golf course data */}
+        
       </DialogContent>
       <DialogActions>
         <Button onClick={onClose}>취소</Button>
-        {golfCourseData.golf_no && (
-          <Button onClick={handleDelete} color="secondary">삭제</Button>
-        )}
-        <Button onClick={handleSave}>{golfCourseData.golf_no ? '수정' : '생성'}</Button>
+        
+        <Button onClick={handleSave}>생성</Button>
+        
       </DialogActions>
     </Dialog>
   );
--- src/Routes/admin/Page/AdminCourse.jsx
+++ src/Routes/admin/Page/AdminCourse.jsx
@@ -9,7 +9,7 @@ import {
   MenuItem,
   Box
 } from '@mui/material';
-import { getCourse, updateCourse, deleteCourse, postCourse } from '../../../services/admin/GolfCourse.js';
+import { getCourse, updateCourse, deleteCourse, postCourse, fetchGolfNames } from '../../../services/admin/GolfCourse.js';
 import CourseModal from '../AdminCourseComponents/CourseModal';
 import '../Style/AdminGlobal.scss';
 
@@ -18,7 +18,7 @@ const AdminCourse = () => {
   const [openModal, setOpenModal] = useState(false);
   const [selectedCourse, setSelectedCourse] = useState(null);
   const [searchTerm, setSearchTerm] = useState('');
-  
+  const [golfNames, setGolfNames] = useState({}); // 골프장 이름을 저장할 객체
 
   const [filterCategory, setFilterCategory] = useState('course_name'); // 필터링할 카테고리 기본값
   // 코스 데이터 가져오기
@@ -34,11 +34,31 @@ const AdminCourse = () => {
     fetchCourses();
   }, []);
 
+  useEffect(() => {
+    // 골프장 이름 데이터 가져오기
+    const fetchGolfNamesData = async () => {
+      const data = await fetchGolfNames();
+      // data를 골프장 번호를 키로 하는 객체로 변환하여 저장
+      const golfNamesObj = data.reduce((acc, golf) => {
+        acc[golf.golf_no] = golf.name;
+        return acc;
+      }, {});
+      setGolfNames(golfNamesObj);
+    };
+
+    fetchGolfNamesData();
+  }, []);
   // DataGrid 컬럼 정의
   
   const columns = [
-    { field: 'course_no', headerName: '코스NO', width: 70 },
-    { field: 'golf_no', headerName: '골프장NO', width: 70 },
+ 
+    { field: 'course_no', headerName: 'NO', width: 70 },
+    { 
+      field: 'golf_no', 
+      headerName: '골프장 이름', 
+      width: 130,
+      valueGetter: (params) => golfNames[params.row.golf_no] || '알 수 없음'
+    },
     { field: 'course_name', headerName: '코스 이름', width: 130 },
     { field: 'greenpee', headerName: '그린피', width: 130 },
     { field: 'golf_time', headerName: '티오프 시간', width: 90 },
@@ -52,17 +72,32 @@ const AdminCourse = () => {
     setSelectedCourse({}); // 새로운 코스 객체 초기화
     setOpenModal(true);
   };
+// "수정" 버튼 클릭 핸들러
+const handleUpdateClick = () => {
+  if (selectedCourse) {
+    setOpenModal(true); // 선택된 코스로 모달 열기
+  } else {
+    alert('수정할 코스를 선택해주세요.');
+  }
+};
 
-  // "수정" 버튼 클릭 핸들러
-  const handleUpdateClick = () => {
-    // 수정 로직 구현 필요
-  };
-
-  // "삭제" 버튼 클릭 핸들러
-  const handleDeleteClick = () => {
-    // 삭제 로직 구현 필요
-  };
-
+// "삭제" 버튼 클릭 핸들러
+const handleDeleteClick = async () => {
+  if (selectedCourse) {
+    if (window.confirm('선택한 코스를 삭제하시겠습니까?')) {
+      try {
+        await deleteCourse(selectedCourse.course_no);
+        // 성공적으로 삭제 후 코스 목록 갱신
+        setCourses(courses.filter(course => course.course_no !== selectedCourse.course_no));
+        setSelectedCourse(null);
+      } catch (error) {
+        console.error('코스 삭제 중 오류 발생', error);
+      }
+    }
+  } else {
+    alert('삭제할 코스를 선택해주세요.');
+  }
+};
   // 검색 입력 핸들러
   const handleSearchChange = (event) => {
     setSearchTerm(event.target.value);
@@ -94,9 +129,9 @@ const AdminCourse = () => {
             onChange={handleFilterCategoryChange}
             label="카테고리"
           >
+            <MenuItem value="course_no">NO</MenuItem>
+            <MenuItem value="golf_no">골프장 이름</MenuItem>
             <MenuItem value="course_name">코스 이름</MenuItem>
-            <MenuItem value="course_no">코스NO</MenuItem>
-            <MenuItem value="golf_no">골프장NO</MenuItem>
             <MenuItem value="greenpee">그린피</MenuItem>
             <MenuItem value="golf_time">티오프 시간</MenuItem>
             <MenuItem value="golf_date">예약 날짜</MenuItem>
@@ -136,14 +171,15 @@ const AdminCourse = () => {
           getRowId={(row) => row.course_no}
           onRowDoubleClick={(params) => setSelectedCourse(params.row)}
         />
-        {selectedCourse && (
-          <CourseModal
-            open={openModal}
-            onClose={() => setOpenModal(false)}
-            course={selectedCourse}
-            onSave={updateCourse}
-          />
-        )}
+       {selectedCourse && (
+              <CourseModal
+                open={openModal}
+                onClose={() => setOpenModal(false)}
+                course={selectedCourse}
+                onUpdate={updateCourse}
+                onPost={postCourse}
+              />
+            )}
       
       </div>
     </div>
